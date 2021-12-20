8e380267b1668ef44adc64c38dcab0235771c65c
Mizuki <mizuki793@users.noreply.github.com>
2021-12-20 21:58:08 +0900

    コンストラクタとしてbuttonクラスを追加

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,14 +1,27 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Button;
 
 
 	@Controller
 	@RequestMapping("/top")
 	public class TopController {
 	 @RequestMapping("/")
-	  public String index() {
+	  public String index(Model model) {
+		Button introduction = new Button("自己紹介","introduction");
+		Button book = new Button("好きな本","book");
+		Button anime = new Button("好きなアニメ","anime");
+		Button movie = new Button("好きな動画","movie");
+
+		model.addAttribute("introduction",introduction);
+		model.addAttribute("book",book);
+		model.addAttribute("anime",anime);
+		model.addAttribute("movie",movie);
+
 	    return "index";
 	 }
 	@RequestMapping("introduction")
---
+++ src/main/java/com/example/demo/entity/Button.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+public class Button {
+
+	public String view_name;
+	public String url;
+
+	public Button(String name,String str_url) {
+		 view_name = name;
+		 url = str_url;
+	}
+}
