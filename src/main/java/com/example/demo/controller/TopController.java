e355d6b7f00ed23b3340809d7e660096e551728f
Mizuki <mizuki793@users.noreply.github.com>
2021-12-21 14:36:59 +0900

    issue#1_buttonをlistとして動かす

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,31 +1,33 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.constants.Button;
-
+import com.example.demo.service.ButtonService;
 
 	@Controller
 	@RequestMapping("/top")
 	public class TopController {
+
+		@Autowired
+		ButtonService buttonService;
+
+
 	 @RequestMapping("/")
 	  public String index(Model model) {
-		Button introduction = new Button("自己紹介","introduction");
-		Button book = new Button("好きな本","book");
-		Button anime = new Button("好きなアニメ","anime");
-		Button movie = new Button("好きな動画","movie");
-
-		model.addAttribute("introduction",introduction);
-		model.addAttribute("book",book);
-		model.addAttribute("anime",anime);
-		model.addAttribute("movie",movie);
-
+		List<Button> buttonList = buttonService.getButtonList();
+		model.addAttribute("buttonList" ,buttonList);
 	    return "index";
 	 }
+
 	@RequestMapping("introduction")
 	 public String introduce() {
 	   return "introduction";
 	}
+
 }
