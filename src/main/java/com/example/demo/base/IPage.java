c42fb5f4fa2b643783f24d4113dce99878017f62
Mizuki <mizuki793@users.noreply.github.com>
2021-12-26 22:47:26 +0900

    PageBaseをパッケージBaseに追加

--- src/main/java/com/example/demo/base/IPage.java
+++ src/main/java/com/example/demo/base/IPage.java
@@ -5,18 +5,5 @@ import org.springframework.ui.Model;
 //インターフェースの実装
 public interface IPage {
 
-	public static String GetHtml(Model model) {
-		return null;
-	}
-}
-
-//baseクラスの実装
-class PageBase implements IPage {
-	//finalの宣言では駄目な理由が分からない。
-	protected String html;
-	protected String title;
-
-	public String GetHtml(Model model) {
-		return html;
-	}
+	public String GetHtml(Model model);
 }
---
+++ src/main/java/com/example/demo/base/PageBase.java
@@ -0,0 +1,13 @@
+package com.example.demo.base;
+
+import org.springframework.ui.Model;
+
+class PageBase implements IPage {
+	//finalの宣言では駄目な理由が分からない。
+	protected String html;
+	protected String title;
+
+	public String GetHtml(Model model) {
+		return html;
+	}
+}
